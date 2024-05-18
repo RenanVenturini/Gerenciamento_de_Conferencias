d8d2dcdb9154fd4e47e3d25c30e38f78ee891221
renan <renanventurinioficial@gmail.com>
2024-05-18 14:22:56 -0300

    Configuração EntityFramework

--- Gerenciamento_Conferencias/Data/Table/Palestra.cs
+++ Gerenciamento_Conferencias/Data/Table/Palestra.cs
@@ -6,7 +6,7 @@ namespace Gerenciamento_Conferencias.Data.Table
     {
         public int Id { get; set; }
         public string Titulo { get; set; }
-        public int DuracaoMinutos { get; set; }
+        public int Duracao { get; set; }
         public int SessaoId { get; set; }
         public Sessao Sessao { get; set; }
     }
